50ea71ed181e144a199edf7e9cb16305f3febead
unknown <18KJerzyk.ACC@lwetb.ie>
2023-10-16 13:03:19 +0100

    q4 done, q5 started

--- recursive.py
+++ recursive.py
@@ -17,3 +17,26 @@ def q2():
 
 def q3():
     term = int(input("Which term of the fibonacci sequence would you like to find?\n --> "))
+
+
+def q4():
+    def sum_of_digits(num):
+        if num == 0:
+            return 0
+        else:
+            return (int(num) % 10) + sum_of_digits(int(num / 10))
+        
+        
+def q5():
+    a = int(input("Enter first number to find their GCD -->"))
+    b = int(input("Enter second number  to find their GCD -->"))
+
+    def euclidean(a, b):
+        AdivideB = a / b
+        AmodB = a % b
+        
+            
+        
+        
+
+    print(sum_of_digits(abs(int(input("Enter a positive integer to get the sum of it's digits of -->")))))	
